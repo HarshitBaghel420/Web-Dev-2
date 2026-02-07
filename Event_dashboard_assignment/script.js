191c70053b1fd621df24a5f411dc55f19aa57048
HARSHIT BAGHEL <harshitbaghel3549@gmail.com>
2026-02-07 11:20:26 +0530

    Update script.js

--- Event_dashboard_assignment/script.js
+++ Event_dashboard_assignment/script.js
@@ -1,44 +1,72 @@
-const inputbox = document.querySelector("#inputbox");
-const addbtn = document.querySelector("#addbtn");
-const list = document.querySelector("#list");
+const inputBox = document.querySelector('#inputBox')
+const addBtn = document.querySelector('#addBtn')
+const list = document.querySelector('#list')
+
+
+let listData = []
 
 function createList() {
-  list.innerHTML = "";
-  listData.forEach((element, i) => {
-    const eachList = document.createElement("div");
-
-    eachList.innerHTML = `<p>${inputbox.value}</p><button onclick='DeleteHandler(${i})'>Delete</button><button onclick='EditHandler(${i})'>Edit</button>`;
-    list.append(eachList);
-    eachList.setAttribute("class", "eachList");
-  });
-}
-
-addbtn.addEventListener("click", () => {
-  let listData = [];
-
-  if (inputbox.value !== "") {
-    listData.push({ listvalue: inputbox.value });
+    list.innerHTML = ''
 
     listData.forEach((element, i) => {
-      const eachList = document.createElement("div");
+        // creating div
+        const eachList = document.createElement('div')
+        // inserting data into div
+        eachList.innerHTML = ` <p>${element.listValue}</p>
+                <button onclick='DeleteHandler(${i})'>Delete</button>
+                <button onclick='EditHandler(${i})'>Edit</button>`
+        list.append(eachList)
+        eachList.setAttribute('class', 'eachList')
+    })
 
-      eachList.innerHTML = `<p>${inputbox.value}</p><button onclick='DeleteHandler(${i})'>Delete</button><button onclick='EditHandler(${i})'>Edit</button>`;
-      list.append(eachList);
-      eachList.setAttribute("class", "eachList");
-    });
-  }
-});
-
-function DeleteHandler(i) {
-  listData.splice(i, 1);
-  list.innerHTML = "";
-  listData.forEach((element, i) => {
-    const eachList = document.createElement("div");
-
-    eachList.innerHTML = `<p>${inputbox.value}</p><button onclick='DeleteHandler(${i})'>Delete</button><button onclick='EditHandler(${i})'>Edit</button>`;
-    list.append(eachList);
-    eachList.setAttribute("class", "eachList");
-  });
+    inputBox.value = ''
 }
 
-function EditHandler(i) {}
+
+let editState = null
+
+addBtn.addEventListener('click', () => {
+    if (inputBox.value !== '' && editState === null) {
+
+        listData.push({
+            listValue: inputBox.value
+        })
+       
+        inputBox.value = ''
+        // console.log(listData)   
+    }else{
+
+        listData[editState].listValue = inputBox.value
+        editState = null
+        addBtn.textContent = 'Add'
+
+
+    }
+
+     createList()
+})
+
+
+function DeleteHandler(i) {
+    listData.splice(i, 1)
+    createList()
+
+}
+
+function EditHandler(i){
+
+    inputBox.value = listData[i].listValue
+    addBtn.textContent = 'Update'
+    editState = i
+    // listData[i].listValue = 
+
+
+}
+
+
+// let arr = [1,2,3,4]
+
+// arr.splice(1,1)
+// console.log(arr)
+
+let x= prompt('enter')
